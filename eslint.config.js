2850b29bd5e3be239e64101398474e8613034630
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-17 18:36:58 -0300

    chore: add .editorconfig file

--- eslint.config.js
+++ eslint.config.js
@@ -5,39 +5,39 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 
 export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module'
-      }
-    },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ],
-      quotes: ['error', 'single'],
-      'jsx-quotes': ['error', 'prefer-double'],
-      semi: ['error', 'always'],
-      'comma-dangle': ['error', 'never'],
-      'react/prop-types': 'off'
-    }
-  }
+	{ ignores: ['dist'] },
+	{
+		files: ['**/*.{js,jsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				ecmaFeatures: { jsx: true },
+				sourceType: 'module'
+			}
+		},
+		settings: { react: { version: '18.3' } },
+		plugins: {
+			react,
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			...react.configs.recommended.rules,
+			...react.configs['jsx-runtime'].rules,
+			...reactHooks.configs.recommended.rules,
+			'react/jsx-no-target-blank': 'off',
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true }
+			],
+			quotes: ['error', 'single'],
+			'jsx-quotes': ['error', 'prefer-double'],
+			semi: ['error', 'always'],
+			'comma-dangle': ['error', 'never'],
+			'react/prop-types': 'off'
+		}
+	}
 ];
--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import { Post } from './Post';
 import './styles.css';
 
 export function App() {
-  return (
-    <div>
-      <Header />
-      <Post
-        author="Victor Badaró"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis reiciendis eaque a quisquam assumenda, accusamus voluptatum recusandae accusantium magni dolorum impedit excepturi laudantium id amet, provident fuga temporibus quaerat at."
-      />
-      <Post
-        author="John Doe"
-        content="Um novo post muito legal."
-      />
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<Post
+				author="Victor Badaró"
+				content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis reiciendis eaque a quisquam assumenda, accusamus voluptatum recusandae accusantium magni dolorum impedit excepturi laudantium id amet, provident fuga temporibus quaerat at."
+			/>
+			<Post
+				author="John Doe"
+				content="Um novo post muito legal."
+			/>
+		</div>
+	);
 }
