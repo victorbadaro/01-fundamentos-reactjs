e91ea9d1eb179a939baa08869962df84204917a8
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-18 20:29:28 -0300

    feat: add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import { Post } from './Post';
 
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
 	return (
 		<div>
 			<Header />
-			<Post
-				author="Victor Badaró"
-				content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis reiciendis eaque a quisquam assumenda, accusamus voluptatum recusandae accusantium magni dolorum impedit excepturi laudantium id amet, provident fuga temporibus quaerat at."
-			/>
-			<Post
-				author="John Doe"
-				content="Um novo post muito legal."
-			/>
+
+			<div className={styles.wrapper}>
+				<Sidebar />
+
+				<main>
+					<Post
+						author="Victor Badaró"
+						content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis reiciendis eaque a quisquam assumenda, accusamus voluptatum recusandae accusantium magni dolorum impedit excepturi laudantium id amet, provident fuga temporibus quaerat at."
+					/>
+					<Post
+						author="John Doe"
+						content="Um novo post muito legal."
+					/>
+				</main>
+			</div>
 		</div>
 	);
 }
