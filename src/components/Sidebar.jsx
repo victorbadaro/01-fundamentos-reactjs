f5df48ffbd8569e5e7e8fca3c932878927cbe073
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-20 18:28:59 -0300

    feat: add avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from '@phosphor-icons/react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,7 +12,7 @@ export function Sidebar() {
 			/>
 
 			<div className={styles.profile}>
-				<img className={styles.avatar} src="https://github.com/victorbadaro.png" />
+				<Avatar src="https://github.com/victorbadaro.png" />
 				<strong>Victor Badaró</strong>
 				<span>Web Developer</span>
 			</div>
