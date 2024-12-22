6d6fb155a950906d5a94fc7749946c07614ca03f
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-12-22 12:07:43 -0300

    feat: add state to storage comments in each post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,14 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
+	const [comments, setComments] = useState([1, 2]);
+
 	const publishedDateFormatted = format(publishedAt, 'dd \'de\' LLLL \'às\' HH:mm\'h\'', {
 		locale: ptBR
 	});
@@ -13,6 +16,11 @@ export function Post({ author, publishedAt, content }) {
 		locale: ptBR,
 		addSuffix: true
 	});
+
+	function handleCreateNewComment() {
+		event.preventDefault();
+		setComments([...comments, comments.length + 1]);
+	}
 
 	return (
 		<article className={styles.post}>
@@ -39,7 +47,7 @@ export function Post({ author, publishedAt, content }) {
 				})}
 			</div>
 
-			<form className={styles.commentForm}>
+			<form className={styles.commentForm} onSubmit={handleCreateNewComment}>
 				<strong>Deixe seu feedback</strong>
 				<textarea placeholder="Deixe um comentário" />
 
@@ -49,9 +57,7 @@ export function Post({ author, publishedAt, content }) {
 			</form>
 
 			<div className={styles.commentsList}>
-				<Comment />
-				<Comment />
-				<Comment />
+				{comments.map(comment => <Comment key={comment} />)}
 			</div>
 		</article>
 	);
